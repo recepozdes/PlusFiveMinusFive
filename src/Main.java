789e0cb55ed2bb222cf7907bb393cfdb84a185a1
recep ozdes <ultraaslan_417@hotmail.com>
2022-05-27 15:36:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int minusFive(int x) {
+        if(x<=0){
+            return x;
+        }else{
+            System.out.print(x+" ");
+            return minusFive(x-5);
+        }
+
+    }
+
+    static int plusFive(int x,int y) {
+        if(x>y){
+            return x;
+        }else{
+            System.out.print(x+" ");
+            return plusFive(x+5,y);
+        }
+
+    }
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz : ");
+        int sayi = scanner.nextInt();
+        //minusFive(sayi);
+        plusFive(minusFive(sayi),sayi);
+    }
+}
